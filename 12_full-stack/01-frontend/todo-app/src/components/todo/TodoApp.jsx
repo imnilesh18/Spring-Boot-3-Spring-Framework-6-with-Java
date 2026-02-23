d4bee7b85596e592edd95edc06d67f502ac3c60f
imnilesh18 <nileshshrivastava7737@gmail.com>
2026-02-23 13:01:30 +0530

    Authentication

--- 12_full-stack/01-frontend/todo-app/src/components/todo/TodoApp.jsx
+++ 12_full-stack/01-frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -38,7 +38,7 @@ export default function TodoApp () {
                   </AuthenticatedRoute>
                 }/>
               
-              <Route path='*' element={ 
+              <Route path='/logout' element={ 
                 <AuthenticatedRoute>
                     <LogoutComponent />
                   </AuthenticatedRoute>
