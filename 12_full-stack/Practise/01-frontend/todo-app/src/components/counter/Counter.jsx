22ef671b58f63c7bb4422365435362c34b582d49
imnilesh18 <nileshshrivastava7737@gmail.com>
2026-08-14 12:00:46 +0530

    added state using useState

--- 12_full-stack/Practise/01-frontend/todo-app/src/components/counter/Counter.jsx
+++ 12_full-stack/Practise/01-frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,22 +1,24 @@
+import { useState } from 'react'
 import './Counter.css'
 
 export default function Counter() {
 
+  // [0, ƒ]
+  // const [firstElt, secondElt] = array
+  const [count, setCount] = useState(0);
+
   function incrementCounterFunction() {
-    console.log('increment clicked')
+    setCount(count + 1)
   }
 
   return ( 
     <div className="Counter">
-      <span className="count">0</span>
+      <span className="count">{count}</span>
       <div>
         <button className="counterButton" 
                 onClick={incrementCounterFunction}      
         >+1</button>
-
       </div>
-      
     </div>
-
   )
 }
