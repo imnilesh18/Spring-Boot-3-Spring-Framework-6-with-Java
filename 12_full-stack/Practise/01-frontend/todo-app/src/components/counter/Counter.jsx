51add0168ef13646633dfceff518375e8c3a584c
imnilesh18 <nileshshrivastava7737@gmail.com>
2026-08-14 11:43:27 +0530

    defined css in JSX

--- 12_full-stack/Practise/01-frontend/todo-app/src/components/counter/Counter.jsx
+++ 12_full-stack/Practise/01-frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,3 +1,5 @@
+import './Counter.css'
+
 export default function Counter() {
 
   function incrementCounterFunction() {
@@ -8,7 +10,10 @@ export default function Counter() {
     <div className="Counter">
       <span className="count">0</span>
       <div>
-        <button className="counterButton" onClick={incrementCounterFunction}>+1</button>
+        <button className="counterButton" 
+                onClick={incrementCounterFunction}      
+        >+1</button>
+
       </div>
       
     </div>
