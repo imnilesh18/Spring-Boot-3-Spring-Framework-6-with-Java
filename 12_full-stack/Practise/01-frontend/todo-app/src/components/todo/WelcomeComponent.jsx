409db2dba9226a9dbe1427e3888babd97d213f99
imnilesh18 <nileshshrivastava7737@gmail.com>
2026-08-22 11:19:12 +0530

    Refactored React Components to Individual JavaScript Modules

---
+++ 12_full-stack/Practise/01-frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -0,0 +1,19 @@
+import {useParams, Link} from 'react-router-dom'
+
+function WelcomeComponent() {
+
+  const {username } = useParams()
+
+  console.log(username)
+
+  return (
+    <div className="WelcomeComponent">
+      <h1>Welcome {username}</h1>
+      <div>
+        Manage Your todos. <Link to="/todos">Go Here</Link>
+      </div>
+    </div>
+  )
+}
+
+export default WelcomeComponent
